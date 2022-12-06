f38f797db7f7e0f083ba1f83cbfac2acba1a747f
Mario Torres <mario.torreslepe@gmail.com>
2022-12-05 21:01:02 -0300

    add search component

--- src/heroes/helpers/index.js
+++ src/heroes/helpers/index.js
@@ -1,2 +1,3 @@
 export * from "./getHeoresByPublisher";
 export * from "./getHeroById";
+export * from './getHeroesByName'
--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,33 +1,74 @@
-import {HeroCard} from "../components";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useForm } from "../../hooks/useForm";
+
+import queryString from "query-string";
+import { getHeroesByName } from "../helpers";
+import { HeroCard } from "../components/HeroCard";
 
 export const SearchPage = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { q = "" } = queryString.parse(location.search);
+
+  const heroes = getHeroesByName(q);
+
+  const showSearch = q.length === 0;
+
+  const showError = q.length > 0 && heroes.length === 0;
+
+  const { searchText, onInputChange } = useForm({ searchText: q });
+
+  const onSearchSubmit = (event) => {
+    event.preventDefault(); //evita el full refreach
+    navigate(`?q=${searchText.toLowerCase().trim()}`);
+  };
+
   return (
     <>
       <h1>Search Page</h1>
       <hr />
       <div className="">
-        <form className="mt-2 d-flex flex-column">
+        <form className="mt-2 d-flex flex-column" onSubmit={onSearchSubmit}>
           <input
             type="text"
             className="form-control"
             placeholder="Busca un heroe"
-            name="seartchText"
+            name="searchText"
             autoComplete="off"
+            value={searchText}
+            onChange={onInputChange}
           />
           <button className="btn btn-outline-warning mt-3">Search</button>
         </form>
 
-        <div className="mt-2">
+        {/* {q === "" ? ( */}
+        {/*   <div className="alert alert-secondary mt-2"> */}
+        {/*     Waiting for search a hero.... */}
+        {/*   </div> */}
+        {/* ) : ( */}
+        {/*   heroes.length === 0 && ( */}
+        {/*     <div className="alert alert-danger mt-2"> */}
+        {/*       There's no results with <b>{q}</b> */}
+        {/*     </div> */}
+        {/*   ) */}
+        {/* )} */}
 
-          <div className="alert alert-secondary">Waiting for search a hero</div>
+        <div
+          className="alert alert-secondary mt-2"
+          style={{ display: showSearch ? "" : "none" }} >
+          Waiting for search a hero....
+        </div>
+        <div
+          className="alert alert-danger mt-2"
+          style={{ display: showError ? "" : "none" }} >
+          There's no results with <b>{q}</b>
+        </div>
 
-          <div className="alert alert-danger">
-            There's no results with <b>XXXXXXXX</b>
-          </div>
-
-          <div className="d-flex">
-            {/* <HeroCard /> */}
-          </div>
+        <div className="d-flex flex-wrap mt-2">
+          {heroes.map((hero) => (
+            <HeroCard key={hero.id} {...hero} />
+          ))}
         </div>
       </div>
     </>
